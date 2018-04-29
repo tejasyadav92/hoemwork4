cbeee6d98ca7b6b64e2396e8d07472231b4447c0
tejasyadav <bheemakanahallit@fontbonne.edu>
2018-04-29 15:44:28 -0500

    Created model class- student

---
+++ src/main/java/com/cloud/model/student.java
@@ -0,0 +1,24 @@
+package com.cloud.model;
+
+public class student {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    String email;
+}
